3189ab4144662f26d185e69cbaf12182eca32690
Sumio K <tap.miwo@gmail.com>
2022-09-08 06:14:39 +0900

    1.6

--- helloruby.rb
+++ helloruby.rb
@@ -1,11 +1,32 @@
-print("Hello, Ruby.\n")
+# print("Hello, Ruby.\n")
 
-print("Hello,RUBY!\n")
+# print("Hello,RUBY!\n")
 
-print("Hello, \"RUBY\"\n")
+# print("Hello, \"RUBY\"\n")
 
-print("Hello \\ Ruby.\n")
+# print("Hello \\ Ruby.\n")
 
-print('Hello,\nRuby\n!\n')
+# print('Hello,\nRuby\n!\n')
 
-print('Hello, \\ \'Ruby\'.')
+# print('Hello, \\ \'Ruby\'.')
+
+# print "Hello, Ruby.\n"
+
+# print "Hello, ", "Ruby", ".", "\n"
+
+# print "Hello"
+# print "Ruby"
+# print "."
+# print "\n"
+
+# puts "Hello, Ruby"
+
+# puts "Hello,","Ruby"
+
+# puts "100"
+# puts 100
+# p "100"
+# p 100
+
+puts "Hello,\n\tRuby."
+p "Hello,\n\tRuby."
