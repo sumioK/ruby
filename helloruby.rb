fadfbaf5debc2eae0efc8dea8665516230a83f60
Sumio K <tap.miwo@gmail.com>
2022-09-08 06:27:07 +0900

    1.9

--- helloruby.rb
+++ helloruby.rb
@@ -28,5 +28,30 @@
 # p "100"
 # p 100
 
-puts "Hello,\n\tRuby."
-p "Hello,\n\tRuby."
+# puts "Hello,\n\tRuby."
+# p "Hello,\n\tRuby."
+
+# puts 1+1
+# puts 2-3
+# puts 5*2
+# puts 100/5
+
+# puts Math.sin(3.1415)
+# puts Math.sqrt(100)
+
+# alphabet= "abcdefg"
+# num= 10
+# age = 18
+# name= "TAKAHASHI"
+# puts alphabet
+# puts num
+# puts name
+# puts age
+
+# x= 10
+# y= 20
+# z=30
+# area= (x*y + y*z + z*x) * 2
+# volume = x * y * z
+# print "表面積=", area,"\n"
+# print "体積=", volume, "\n"
