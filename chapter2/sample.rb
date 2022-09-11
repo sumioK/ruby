3bf9e0e146f5c7782056d43923102082037f74e0
Sumio K <tap.miwo@gmail.com>
2022-09-12 06:10:14 +0900

    4-6

--- chapter2/sample.rb
+++ chapter2/sample.rb
@@ -7,3 +7,22 @@
 # first, *second, third = 1, 2, 3, 4, 5
 # puts third
 
+# a, b = 0, 1
+# tmp = a
+# a = b
+# b = tmp
+# p [a, b]
+
+# a, b = 0, 1
+# a, b = b, a
+# p [a, b]
+
+# ary = [1, 2]
+# a, b = ary
+# p a
+# p b
+
+# ary = [1, 2]
+# a, =ary
+# p a
+
