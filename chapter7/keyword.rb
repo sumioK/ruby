d6b0de14e2d2bfc1b715e9485122c1cc2a04d756
Sumio K <tap.miwo@gmail.com>
2022-09-13 21:37:24 +0900

    7.4.1

--- chapter7/keyword.rb
+++ chapter7/keyword.rb
@@ -26,14 +26,26 @@
 # p meth(x: 3, z: 2, v: 4, l: 5)
 
 
-def area(x: 0, y: 0, z:0)
-    xy = x * y
-    yz = y * z
-    zx = z * x
-    (xy + yz+ zx) * 2
+# def area(x: 0, y: 0, z:0)
+#     xy = x * y
+#     yz = y * z
+#     zx = z * x
+#     (xy + yz+ zx) * 2
+# end
+
+# p area(x: 4, y: 3, z: 2)
+# args1 = {x: 1, y: 3, z: 4}
+# p args1
+# p area(**args1)
+
+def foo(a, b, c)
+    a + b + c
 end
 
-p area(x: 4, y: 3, z: 2)
-args1 = {x: 1, y: 3, z: 4}
-p args1
-p area(**args1)
+p foo(1, 2, 3)
+
+args1 = [2, 3]
+p foo(1, *args1)
+
+args2 = [1, 2, 3]
+p foo(*args2)
