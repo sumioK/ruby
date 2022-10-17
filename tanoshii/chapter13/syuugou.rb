aeda4a08b3d9c7ac0dc910302988b66644af7292
Sumio K <tap.miwo@gmail.com>
2022-10-18 06:39:22 +0900

    unshift

--- tanoshii/chapter13/syuugou.rb
+++ tanoshii/chapter13/syuugou.rb
@@ -6,9 +6,9 @@ p (ary1 | ary2)
 p (ary1 - ary2)
 
 # 配列の先頭・末尾を操作するメソッド
-# unshift <-> push
-# shift <-> pop
-# first <-> last
+# 先頭に追加unshift <-> 末尾に追加push
+# 先頭から取り出すshift <-> 末尾から取り出すpop
+# 先頭を参照first <-> 末尾を参照last
 alpha = ["a", "b", "c", "d","e"]
 p alpha.push("f")
 p alpha.shift
