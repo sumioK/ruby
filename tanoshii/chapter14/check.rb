6b004e462c0d55923e32bb1328bea65985940357
Sumio K <tap.miwo@gmail.com>
2022-11-04 21:33:40 +0900

    chomp

--- tanoshii/chapter14/check.rb
+++ tanoshii/chapter14/check.rb
@@ -7,3 +7,14 @@ p "aaa" != "aaa"
 str = "高橋: タカハシ: 1234567:000-111-222"
 column = str.split(":")
 p column
+
+# 改行文字を取り除くメソッド
+str = "abcde"
+newstr = str.chop
+p newstr
+
+str2 = "abcde\n"
+newstr = str2.chop
+p newstr
+newstr = str2.chomp
+p newstr
