1a672803e19293e03738697bc7a2f42c6cdc2b4f
Sumio K <tap.miwo@gmail.com>
2022-11-05 09:58:05 +0900

    meth

--- tanoshii/chapter14/meth.rb
+++ tanoshii/chapter14/meth.rb
@@ -13,3 +13,15 @@ p s.reverse
 
 # 文字列の先頭と末尾の空白を消す
 p  " Thank you . ".strip
+
+# アルファベットを大文字に
+p "hello, Ruby".upcase
+# アルファベットを小文字に
+p "HELLO, RUBY".downcase
+# 最初の文字を大文字に、その他を小文字に
+p "heLlo, ruBY".capitalize
+
+# 文字の置き換え .tr("a", "b")aをbに置き換える
+p "あいうえお".tr("い", "イ")
+p "あいうえお".tr("あ-お", "ア-オ")
+
