ace9c29a5c3cf7bc5056302c61ee9c99cdf23f7a
Sumio K <tap.miwo@gmail.com>
2022-11-06 10:48:34 +0900

    hash

---
+++ tanoshii/chapter15/hash_base.rb
@@ -0,0 +1,15 @@
+# 配列
+person = Array.new
+person[0] = "田中一郎"
+person[1] = "佐藤次郎"
+person[2] = "木村三郎"
+
+p person[1]
+
+# ハッシュ
+person = Hash.new
+person["tanaka"] ="田中一郎"
+person["satou"] = "佐藤次郎"
+person["kimura"] = "木村三郎"
+
+p person["kimura"]
